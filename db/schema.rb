1ffdecb24ea8faa07e9c8ab4eb03705295ce37d7
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-02-25 16:45:45 -0700

    comments table created

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170225225616) do
+ActiveRecord::Schema.define(version: 20170225234453) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "winner"
+    t.integer  "loser"
+    t.string   "comment"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
+  end
 
   create_table "users", force: :cascade do |t|
     t.integer  "census_id"
@@ -22,4 +32,5 @@ ActiveRecord::Schema.define(version: 20170225225616) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "comments", "users"
 end
---
+++ spec/factories/comments.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :comment do
+    winner 1
+    loser 1
+    comment "MyString"
+    user nil
+  end
+end
