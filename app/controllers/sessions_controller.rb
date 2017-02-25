cedd035b95f42c4252e3b44745a7266e8fadb34a
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-02-25 16:35:39 -0700

    user can log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -12,9 +12,9 @@ class SessionsController < ApplicationController
         redirect_to root_path
       end
     end
-    
+
     def destroy
-      session[:user_id] = nil
+      session.clear
       redirect_to root_path
     end
 
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :user do
-    
+
+    census_id 14
+
+    token ENV["census_token"]
   end
 end
