1945d5e9229ca3083365892637076d0b87ec6bb5
Noah Berman <noah@noahberman.org>
2017-02-26 15:29:34 -0700

    comments saving to database

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,7 @@
 class ApiController < ActionController::API
-
+  helper_method :current_user
+  
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
 end
