d36747f1646c0848c8a9bc3bb75ce2767e4e9584
Noah Berman <noah@noahberman.org>
2017-02-26 16:16:58 -0700

    loops in code of arms service

--- app/services/code_of_arms_service.rb
+++ app/services/code_of_arms_service.rb
@@ -1,5 +1,14 @@
 module CodeOfArmsService
-
+  
+  def self.get_solution_pairs(all_solutions)
+    most_recent_winner = Comment.where(user_id: current_user.id).last.winner
+    all_solutions.each do |solution|
+      if 
+        
+      end
+    end
+  end
+  
   def self.get_response
     response = Faraday.get('https://code-of-arms.herokuapp.com/api/v1/exercises')
     exercise_object = JSON.parse(response.body, symbolize_names: true)
