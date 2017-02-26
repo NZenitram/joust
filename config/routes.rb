33fa7f9b7c2a1780f253b8ec796b2de5c0c0d63c
Nicholas Martinez <nicholasmartinez@gmail.com>
2017-02-25 18:31:28 -0700

    post route for API created

--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,10 @@ Rails.application.routes.draw do
   get 'signout', to: 'sessions#destroy', as: 'signout'
 
   post '/', to: 'home#create'
+
+  namespace :api do
+    namespace :v1 do
+      resources :comments, only: [:create]
+    end
+  end
 end
