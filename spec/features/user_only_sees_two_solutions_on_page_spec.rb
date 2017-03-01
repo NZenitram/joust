1f978da16bf09c6cefd421f138d62219cc2692a9
Anna Dolan <anna.dolan@gmail.com>
2017-02-28 18:28:39 -0700

    Change tests to pass

--- spec/features/user_only_sees_two_solutions_on_page_spec.rb
+++ spec/features/user_only_sees_two_solutions_on_page_spec.rb
@@ -3,10 +3,13 @@ require 'rails_helper'
 describe 'when a user visits the homepage' do
   context 'the user sees solutions' do
     it 'only sees two solutions' do
+      user = create(:user)
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
       visit '/'
 
-      expect(page).to have_selector('.vote-button', count: 2)
+      expect(page).to have_button('Vote', count: 2)
     end
   end
 end
