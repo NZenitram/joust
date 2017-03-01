782198399aa4776a23941e2a06a9754c6b7ad9c7
Anna Dolan <anna.dolan@gmail.com>
2017-02-28 21:31:19 -0700

    Add validations to comment

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,6 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
+  describe "validations" do
+    context "invalid attributes" do
+      it "is invalid without a census id" do
+        user = create(:user)
+        comment = Comment.new(winner: 1, loser: 2, user: user)
+        expect(comment).to be_invalid
+      end
+    end
+
+    context "valid attributes" do
+      it "is valid with all attributes" do
+        user = create(:user)
+        comment = Comment.new(winner: 1, loser: 2, user: user, comment: 'blah')
+        expect(comment).to be_valid
+      end
+    end
+  end
+
   describe "relationships" do
     it "belongs to a user" do
       comment = create(:comment)
